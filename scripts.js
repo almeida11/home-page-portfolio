e8aff4347b5a3d6aa03ad68f039b6d5be85258b6
Mateus Almeida <mateus.almeidagf@gmail.com>
2023-01-26 23:21:07 -0300

    feat: add meta ads
    add for config pixel from meta

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $(".navbar").addClass("sticky");
-        }else{
+        } else {
             $(".navbar").removeClass("sticky");
         }
     });
@@ -10,3 +10,22 @@ $(document).ready(function(){
 //não está funcionando ao rolar o scroll // avaliar o que está errado
 
 //add funcionalidades dos botoes iniciais
+
+// Meta Pixel Code -->
+!function (f, b, e, v, n, t, s) {
+    if (f.fbq) return; n = f.fbq = function () {
+        n.callMethod ?
+        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+    n.queue = []; t = b.createElement(e); t.async = !0;
+    t.src = v; s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+}(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '903000894392218');
+fbq('track', 'PageView');
+<noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=903000894392218&ev=PageView&noscript=1"
+/></noscript>
+// End Meta Pixel Code -->
